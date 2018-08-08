ea91f1339e9f85ce41e15d1f92f8dc977c596536
TheChosenHero <thechosenherotch@gmail.com>
2018-08-08 15:29:12 -0500

    revert Revert_Class
    reverted the Revert_Class from previous commit to get rid of that huge
    nasty error that was messing with my amazing program that i am writing
    here

--- GitHubTest/src/Revert_Class.java
+++ GitHubTest/src/Revert_Class.java
@@ -5,13 +5,4 @@ public class Revert_Class {
 		String name = "revertclass";
 		System.out.println(name);
 	}
-	public void method() {
-		System.out.println("the name of this class is Revert_Class");
-		//this is the method that I will be trying to revert
-		//it looks really cool rn but man does it suck and contain this massive error that i am about to write
-		this is my massive error
-		break
-		return
-				new
-	}
 }

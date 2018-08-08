8a5ce0c4a1e6dc0af056317d0cc795bf9c9fcc1d
TheChosenHero <thechosenherotch@gmail.com>
2018-08-08 15:26:25 -0500

    new method
    contains massive error that i will be trying to revert from to get rid
    of it

--- GitHubTest/src/Revert_Class.java
+++ GitHubTest/src/Revert_Class.java
@@ -5,4 +5,13 @@ public class Revert_Class {
 		String name = "revertclass";
 		System.out.println(name);
 	}
+	public void method() {
+		System.out.println("the name of this class is Revert_Class");
+		//this is the method that I will be trying to revert
+		//it looks really cool rn but man does it suck and contain this massive error that i am about to write
+		this is my massive error
+		break
+		return
+				new
+	}
 }
